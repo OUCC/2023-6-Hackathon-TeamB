15d6de76d2cf9eea7c7859151f8240a4cf16939c
MrMocchy <patronummocchy@tiara.ocn.ne.jp>
2023-06-15 22:24:47 +0900

    入力からTextの変更

---
+++ Assets/Scripts/TextUpdater.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUpdater : MonoBehaviour
+{
+
+    public GameObject nameTextObject;
+    public GameObject messageTextObject;
+
+    private Text namec;
+    private Text messagec;
+
+    public GameObject inputFieldB;
+    private InputField InputFieldc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        namec = nameTextObject.GetComponent<Text>();
+        messagec = messageTextObject.GetComponent<Text>();
+        InputFieldc = inputFieldB.GetComponent<InputField>();
+
+        messagec.text = "Started";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMessage(string author, string message)
+	{
+        namec.text = author;
+        messagec.text = message;
+	}
+
+    public void OnInputTextSubmit(string text)
+	{
+        SetMessage(author: "A", message: text);
+	}
+    public void OnInputButtonClick()
+	{
+
+        SetMessage(author: "B", message: InputFieldc.text);
+	}
+
+}
