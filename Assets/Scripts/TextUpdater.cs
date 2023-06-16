0825dda00274e1f5ecc5245f024e341b047d2136
MrMocchy <patronummocchy@tiara.ocn.ne.jp>
2023-06-16 22:18:10 +0900

    一文字ずつ順番にテキストが表されるよう変更

--- Assets/Scripts/TextUpdater.cs
+++ Assets/Scripts/TextUpdater.cs
@@ -13,41 +13,69 @@ public class TextUpdater : MonoBehaviour
     private Text namec;
     private Text messagec;
 
+    // 順番に出現するテキストのアニメーション用
+    private string fullMessage = "";
+    private float textAnimTimer;
+    public float textAnimSpeed = 10;
+
+    //デバッグ用
     public GameObject inputFieldB;
     private InputField inputFieldc;
+
+    // 次以降に表示するメッセージ
+    public List<MessageData> futureMessages = new List<MessageData>();
 
     // Start is called before the first frame update
     void Start()
     {
+        // コンポーネント取得
         namec = nameTextObject.GetComponent<Text>();
         messagec = messageTextObject.GetComponent<Text>();
+
         inputFieldc = inputFieldB.GetComponent<InputField>();
 
-        messagec.text = "Started";
+        // デバッグ用のメッセージ
+        futureMessages.Add(new MessageData("A", "aaa"));
+        futureMessages.Add(new MessageData("B", "bbbbbbb"));
+        futureMessages.Add(new MessageData("C", "ccccccccccccccc"));
+        futureMessages.Add(new MessageData("D", "dddddddddddddddddddddd"));
+        futureMessages.Add(new MessageData("E", "eeeeeeeeeeeeeeeeeeeeeeeeeeeee"));
+
     }
 
-    public List<MessageData> futureMessages = new List<MessageData>();
 
     // Update is called once per frame
     void Update()
     {
-
+        // テキストを一文字ずつ順番に表示させる
+        textAnimTimer += Time.deltaTime;
+        messagec.text = fullMessage.Substring(0,Mathf.Min(fullMessage.Length, Mathf.FloorToInt(textAnimTimer * textAnimSpeed)));
     }
 
 
     public void DisplayNextMessage()
 	{
+        //まだアニメーション中なら表示を完了する
+        if(fullMessage.Length > Mathf.FloorToInt(textAnimTimer * textAnimSpeed))
+		{
+            textAnimTimer = fullMessage.Length * textAnimSpeed;
+            return;
+        }
+
+        //次のメッセージが未決定の場合はなにもしない
         if (futureMessages.Count == 0)
             return;
 
+        //新しいメッセージに変更
+
+        textAnimTimer = 0;
+        //次のメッセージをリストから取り出して更新
         MessageData md = futureMessages[0];
         futureMessages.RemoveAt(0);
-
         namec.text = md.author;
-        messagec.text = md.message;
-
+        fullMessage = md.message;
+        //履歴に追加
         History.list.Add(md);
-
 	}
 
     public void SetMessage(MessageData md)
